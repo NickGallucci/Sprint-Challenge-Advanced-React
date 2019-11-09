2584d7351a490e2e77df3e764594c51d0268c2a9
CryptoN999 <nicholasgallucci4@gmail.com>
2019-11-08 19:22:15 -0800

    stretch finished

--- client/src/hooks/useLocalStorage.js
+++ client/src/hooks/useLocalStorage.js
@@ -1,17 +1,17 @@
 import { useState } from 'react';
 
 function useLocalStorage(key, initialValue) {  
-    const [value, setValue] = useState(() => {
-        const item = window.localStorage.getItem(key);
-        return item ? JSON.parse(item) : initialValue;
-    });  
+    const [value, setValue] = useState(() => { 
+        const item = window.localStorage.getItem(key); 
+        return item ? JSON.parse(item) : initialValue; 
+    });   
 
-    const customSetter = value => {
-        setValue(value);
-        window.localStorage.setItem(key, JSON.stringify(value)) 
-    };
-
-    return [value, customSetter]
-}
+    const customSetter = value => { 
+        setValue(value); 
+        window.localStorage.setItem(key, JSON.stringify(value))  
+    }; 
  
-export default useLocalStorage; 
+    return [value, customSetter] 
+} 
+  
+export default useLocalStorage;

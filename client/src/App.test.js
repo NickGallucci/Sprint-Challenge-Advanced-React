5117bb85ae756251f0d8ad6c27b498ef1c677b14
CryptoN999 <nicholasgallucci4@gmail.com>
2019-11-12 12:52:45 -0800

    added tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,28 +1,26 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import App from './App'
+import * as rtl from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect' 
+
 
 test('Renders without Crashing', () => {
   render(<App />);
 })
 
-// import React from 'react'
-// import * as rtl from '@testing-library/react'
-// import '@testing-library/jest-dom/extend-expect' 
-// import App from './App'
+afterEach(rtl.cleanup) 
 
-// afterEach(rtl.cleanup) 
+test(`Renders without crashing, has class name of 'App'`, () => {
+  const wrapper = rtl.render(<App />);
+  const element = wrapper.getByTestId("app-test-id"); 
+  expect(element).toHaveClass('App');
+}); 
 
-// test(`Renders without crashing, has class name of 'App'`, () => {
-//   const wrapper = rtl.render(<App />);
-//   const element = wrapper.getByTestId("app-test-id"); 
-//   expect(element).toHaveClass('App');
-// }); 
-
-// test('toggle loads dark mode', () => {
-//   const { getByTestId } = render(<App/>);
-//   getByTestId("toggle-element");
-// })
+test('toggle loads dark mode', () => {
+  const { getByTestId } = render(<App/>);
+  getByTestId("toggle-element");
+})
 
 // test(' Players card contains text', () => {
 //   const players = render(<App />);
